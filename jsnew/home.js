f1d7608f4b31dad955965f00609d2a0516de2e22
unknown <harshitparikh2000@gmail.com>
2021-03-02 15:07:43 +0530

    product management code added

--- jsnew/home.js
+++ jsnew/home.js
@@ -1,69 +1,65 @@
-function removeTopBarClass(){
-	$('#customers').removeClass('active_link');
-	$('#dashboard').removeClass('active_link');
-	$('#products').removeClass('active_link');
-	$('#dashboardDiv').hide();
-	$('#productDiv').hide();
-	$('#customerDiv').hide();
-}
 function removeSideBarClass(){
 	$('#pro-man').hide();
 	$('#side-dashboard').removeClass('active_menu_link');
 	$('#side-pro-man').removeClass('active_menu_link');
 }
 
-$('#products').click(() => {
-  $('#dashboardDiv').hide();
-  $('#productDiv').show();
-  $('#customerDiv').hide();
-  $('#products').addClass('active_link');
-  $('#dashboard').removeClass('active_link');
-  $('#customers').removeClass('active_link');
-  removeSideBarClass();
-});
-
-$('#dashboard').click(() => {
-  $('#dashboardDiv').show();
-  $('#productDiv').hide();
-  $('#customerDiv').hide();
-  $('#products').removeClass('active_link');
-  $('#customers').removeClass('active_link');
-  $('#dashboard').addClass('active_link');
-  removeSideBarClass();
+$('#side-dashboard').click(() => {
   $('#side-dashboard').addClass('active_menu_link');
-});
-
-$('#customers').click(() => {
-  $('#customerDiv').show();
-  $('#dashboardDiv').hide();
-  $('#productDiv').hide();
-  $('#customers').addClass('active_link');
-  $('#dashboard').removeClass('active_link');
-  $('#products').removeClass('active_link');
-  removeSideBarClass();
+  $('#side-pro-man').removeClass('active_menu_link');
+  $('#side-cus-man').removeClass('active_menu_link');
+  $('#dashboardDiv').show();
+  $('#pro-man').hide();
+  $('#cus-man').hide();
 });
 
 $('#side-pro-man').click(() => {
-  removeTopBarClass();
-  $('#side-dashboard').removeClass('active_menu_link');
   $('#pro-man').show();
+  $('#dashboardDiv').hide();
+  $('#cus-man').hide();
   $('#side-pro-man').addClass('active_menu_link');
+  $('#side-dashboard').removeClass('active_menu_link');
+  $('#side-cus-man').removeClass('active_menu_link');
 });
 
-$('#side-dashboard').click(() => {
-  removeTopBarClass();
-  $('#side-dashboard').addClass('active_menu_link');
-  $('#dashboardDiv').show();
-  $('#dashboard').addClass('active_link');
-  $('#side-pro-man').removeClass('active_menu_link');
+$('#side-cus-man').click(() => {
   $('#pro-man').hide();
+  $('#dashboardDiv').hide();
+  $('#cus-man').show();
+  $('#side-cus-man').addClass('active_menu_link');
+  $('#side-pro-man').removeClass('active_menu_link');
+  $('#side-dashboard').removeClass('active_menu_link');
 });
-
-
+$('#addCategory').click(() =>{
+  if(($('#subCategory').val() == 'Select' && $('#catName').val() == '') || ($('#subCategory').val() != 'Select' && $('#catName').val() != '')){
+    alert('select or add new category');
+  }else{
+    if($('#subCategory').val() == 'Select')
+    {
+      var category = $('#catName').val();
+    } else{
+      var category = $('#subCategory').val();
+    }
+    firebase.database().ref('users/'+ localStorage.getItem('user') + '/subCategory/' + category + '/' + $('#proName').val()).set({
+      productName: $('#proName').val(),
+      price: $('#price').val(),
+      mrp: $('#mrp').val(),
+      description: $('#description').val()
+    });
+    alert('product added');
+    $('#proName').val('');
+    $('#price').val('');
+    $('#mrp').val('');
+    $('#description').val('');
+    $('#catName').val('');
+    $('#subCategory').val('Select');
+  }
+});
   auth.onAuthStateChanged(function(user){
     if(user){
 
       console.log(user.displayName);
+      localStorage.setItem('user',user.displayName);
 
 	  firebase.database().ref('users/').once('value').then(function(snapshot){
         if(snapshot.hasChild(user.displayName)){
@@ -93,19 +89,30 @@ $('#side-dashboard').click(() => {
       $('#product-user').text(user.displayName);
       
       firebase.database().ref('users/'+ user.displayName).once('value').then(function(snapshot){
-        if(snapshot.hasChild('products')){
-          alert('product exist');
+        if(snapshot.hasChild('subCategory')){
+          firebase.database().ref('users/'+ user.displayName + '/subCategory').on("value",(snapshot) => {
+            $('#subCategory')
+            .find('option')
+            .remove()
+            .end()
+            $('#subCategory').append(new Option('Select', 'Select'));              
+            console.log(Object.keys(snapshot.val()));
+            Object.keys(snapshot.val()).forEach(element => {
+              $('#subCategory').append(new Option(element, element));              
+            });
+          });
         } else{
-          $('#product-category').text('Please add atleast one product');
+          $('#product-status').text('Add Your First Category of Product.');
         }
       });
+
+
 
         console.log('Active user ' + user.displayName);
         document.title = user.displayName;
         document.getElementById('user').innerHTML = user.displayName;
         document.getElementById('cmp_name').innerHTML = user.displayName;
         document.getElementById('logo').src = user.photoURL;
-        document.getElementById('profile').src = user.photoURL;
 
     } else{
         console.log('No active use \n Please login');
